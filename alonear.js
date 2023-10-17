f484c8e6ee3a11cac0263f7bb291a021c5cc26b5
ssrivastav11 <112320345+ssrivastav11@users.noreply.github.com>
2023-10-17 19:31:54 +0530

    Create alonear.js

---
+++ alonear.js
@@ -0,0 +1,6 @@
+function contactus()
+{
+    document.getElementById('bodyi').classList.toggle('blur')
+    document.getElementById('contact').classList.toggle("hidden")
+    document.getElementById('idk').classList.toggle("hidden")
+}
